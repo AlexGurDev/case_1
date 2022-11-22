3bc57f19a91247fc8d652c3c9ad7294ddc71f4a1
Alex <AlexGurDev@gmail.com>
2022-11-22 23:48:43 +0300

    add object to indexjs

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-let number = 4;
-console.log(4/0);
-console.log('string' / 4);
+const numberOfFilms = +prompt('How many movies did you see?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
 
+    },
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const a = prompt('What did you watch the last movie?', '');
+const b = prompt('How are you value one?', '');
+const c = prompt('What did you watch the last movie', '');
+const d = prompt('How are you value one?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
